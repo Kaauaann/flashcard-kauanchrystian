1482214a6a8a773108ff3af179b6432fa5bcdd79
ka <00001101494359sp@al.educacao.sp.gov.br>
2024-11-28 18:05:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+
+criaCartao(
+    'futebol',
+    'qual é o melhor time do brasil ?',
+    'corinthians'
+)
+
+criaCartao(
+    'futebol',
+    'qual time não vai ser rebaixado?',
+    'o corinthians'
+)
+
+criaCartao(
+    'futebol',
+    'em que ano o corinthians foi criado?',
+    '1910'
+)
+
+criaCartao(
+    'futebol',
+    'quantos mundial o corinthians tem?',
+    'o corinthians tem 2 mundial'
+)
+
+criaCartao(
+    'futebol',
+    'quantas copa do brasil o corinthians tem?',
+    'o corinthias tem 3 copa do brasil '
+)
+
+criaCartao(
+    'futebol',
+    'contra quem o corinthians ganhou o mundial de 2012?',
+    '2012'
+)
